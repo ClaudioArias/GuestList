27f15fc5f73bb142afa371518d36e3df45fb61e6
Tito <tito>
2023-02-05 22:15:04 -0400

    Initial Commit

---
+++ GuestList/GuestListApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuestListApp.swift
+//  GuestList
+//
+//  Created by Tito on 05/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct GuestListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
